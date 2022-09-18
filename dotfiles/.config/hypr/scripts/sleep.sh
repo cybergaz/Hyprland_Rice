866c009ed12605f982f8cd80bbf2c0cf20d0f8c2
cybergaz <kkanttechy@gmail.com>
2022-09-18 23:19:03 +0530

    updated once again

--- dotfiles/.config/hypr/scripts/sleep.sh
+++ dotfiles/.config/hypr/scripts/sleep.sh
@@ -2,7 +2,7 @@
 swayidle -w \
                 timeout 120 'temp=$(brightnessctl g); brightnessctl set $((temp / 2))' \
                     resume 'temp=$(brightnessctl g); brightnessctl set $((temp * 2))' \
-                timeout 130 "$HOME/.config/hypr/scripts/lock.sh & sleep 1 && hyprctl dispatch dpms off" \
+                timeout 300 "$HOME/.config/hypr/scripts/lock.sh & sleep 1 && hyprctl dispatch dpms off" \
                     resume 'hyprctl dispatch dpms on' \
                 
 
